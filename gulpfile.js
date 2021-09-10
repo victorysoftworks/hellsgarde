791bdc191c9ed638dcfd023d7b1875aa9af6fb06
Victory Softworks <sersavictory@gmail.com>
2021-09-10 00:10:50 -0400

    Added gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+const gulp = require('gulp')
+const concat = require('gulp-concat-util')
+const dedupe = require('gulp-dedupe')
+
+const files = [
+  './vendor/*.js',
+  './src/behaviors/behavior.js',
+  './src/components/component.js',
+  './src/effects/effect.js',
+  './src/entities/entity.js',
+  './src/*/**/*.js',
+  './src/index.js'
+]
+
+gulp.task('build', async function() {
+  gulp.src(files)
+      .pipe(dedupe())
+      .pipe(concat('hellsgarde.js'))
+      .on('error', console.error.bind(console))
+      .pipe(gulp.dest('dist'))
+})
