8f175cd897c0740bcd9268e0a5c762d8a341718e
Victory Softworks <sersavictory@gmail.com>
2021-09-17 00:13:10 -0400

    Added first 128 characters of ASCII sprite sheet

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@
 const config = {
   type: Phaser.AUTO,
   width: 800, // 25 tiles wide
-  height: 576, // 18 tiles tall
+  height: 608, // 19 tiles tall
   backgroundColor: '#201f1b',
   scene: {
     preload: preload,
@@ -14,11 +14,21 @@ const config = {
 }
 
 function preload() {
-  
+  this.load.json('ascii', './data/ascii.json')
+  this.load.spritesheet('ascii', 
+    './data/ascii.png',
+    {
+      frameWidth: 32,
+      frameHeight: 32
+    }
+  )
 }
 
 function create() {
-  
+  const sprites = this.cache.json.get('ascii')
+  const x = 12 * 32;
+  const y = 9 * 32;
+  this.add.image(x, y, 'ascii', sprites['ROGUE']).setOrigin(0, 0).setTint(0xbe90d4)
 }
 
 const game = new Phaser.Game(config)
