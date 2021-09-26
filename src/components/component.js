01f95920370f8d3520ae010a2a7e4572e97e0d4d
Victory Softworks <sersavictory@gmail.com>
2021-09-25 20:19:46 -0400

    Added base component class

--- src/components/component.js
+++ src/components/component.js
@@ -1,1 +1,35 @@
-class Component { }
+/******************************************************************************
+ * The base class from which all component classes are dervied.
+ *****************************************************************************/
+
+class Component {
+
+  /****************************************************************************
+   * Constructor.
+   * 
+   * @param {number} priority Order in which component should be processed
+   ***************************************************************************/
+  
+  constructor(priority) {
+    this.priority = priority
+  }
+
+  /****************************************************************************
+   * Handles events broadcast to the component.
+   * 
+   * @abstract
+   * @param {Event} event Event to handle
+   ***************************************************************************/
+
+  receive(event) { }
+
+  /****************************************************************************
+   * Handles queries made to the component.
+   * 
+   * @abstract
+   * @param {Query} query Query to handle
+   ***************************************************************************/
+
+  query(query) { }
+
+}
