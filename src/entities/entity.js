c1da7f5160f0bea4c81271a2e93ddc5c49e2d223
Victory Softworks <sersavictory@gmail.com>
2021-09-25 20:53:42 -0400

    Added basic entity class code

--- src/entities/entity.js
+++ src/entities/entity.js
@@ -1,1 +1,36 @@
-class Entity { }
+class Entity {
+
+  constructor() {
+    this.id = '_' + Math.random().toString(32)
+    this.components = []
+  }
+
+  addComponent(component) {
+    component.owner = this
+
+    this.components.unshift(component)
+  }
+
+  removeComponent(component) {
+    this.components = this.components.filter(c => ! Object.is(c, component))
+  }
+
+  receive(type, params = {}) {
+    const event = { type, params }
+    
+    this.components.sort((a, b) => b.priority > a.priority)
+
+    this.components.forEach(c => c.receive(event))
+  }
+
+  query(type, result = null, params = {}) {
+    const query = { type, result, params }
+
+    this.components.sort((a, b) => b.priority > a.priority)
+
+    this.components.forEach(c => c.query(query))
+
+    return query.result
+  }
+
+}
