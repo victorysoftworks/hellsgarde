66e35fdf2e50020891651338acc0d211f3460553
Victory Softworks <sersavictory@gmail.com>
2021-10-16 10:37:00 -0400

    Added PostMessageEffect class

---
+++ src/effects/post-message.js
@@ -0,0 +1,32 @@
+/******************************************************************************
+ * The PostMessage effect posts a message to the output queue.
+ *****************************************************************************/
+
+ class PostMessageEffect extends Effect {
+
+  /****************************************************************************
+   * Constructor.
+   * 
+   * @param {string} message Message to post
+   ***************************************************************************/
+  
+  constructor(message) {
+    this.message = message
+  }
+
+  /****************************************************************************
+   * Executes the effect.
+   * 
+   * @param {Entity} source Source entity
+   * @param {Entity} target Target entity
+   ***************************************************************************/
+
+  execute(source, target) {
+
+    // TODO: Move message output logic into the Game object
+
+    const messageBox = document.querySelector('[data-message]')
+    messageBox.textContent = this.message
+  }
+
+}
