edede1f660ce90b5ac86670b3fb82cde0a645a57
Victory Softworks <sersavictory@gmail.com>
2021-10-16 23:27:40 -0400

    Moved turn processing code into its own method

--- src/scene/main.js
+++ src/scene/main.js
@@ -28,6 +28,62 @@ class MainScene extends Phaser.Scene {
   }
 
   update() {
+    this.processTurn()
+  }
+
+  render() {
+    const rogue = Game.entityManager.getRogue()
+    const position = rogue.query('position')
+    const minX = position.x - ((this.cameraWidth - 1) / 2)
+    const maxX = position.x + ((this.cameraWidth - 1) / 2)
+    const minY = position.y - ((this.cameraHeight - 1) / 2)
+    const maxY = position.y + ((this.cameraHeight - 1) / 2)
+    
+    let screenX = 0
+    let screenY = 0
+
+    for (let y = minY; y <= maxY; y++) {
+      if (y >= 0 && y < Game.map.terrain.length) {
+        for (let x = minX; x <= maxX; x++) {
+          if (x >= 0 && x < Game.map.terrain[0].length) {
+            let tint
+    
+            switch (Game.map.terrain[y][x]) {
+              case 250:
+                tint = 0x3a3a3a
+                break
+              case 43:
+                tint = 0xa66b3a
+                break
+              default:
+                tint = 0x666666
+                break
+            }
+            
+            if (x !== position.x || y !== position.y) {
+              this.add.image(screenX * this.frameWidth, screenY * this.frameHeight, 'ascii', Game.map.terrain[y][x]).setOrigin(0, 0).setTint(tint)
+            }
+
+            const entityToRender = Game.entityManager.getRenderableEntityAtPosition(x, y)
+
+            if (entityToRender) {
+              let g = entityToRender.query('glyph')
+              let c = entityToRender.query('color')
+      
+              this.add.image(screenX * this.frameWidth, screenY * this.frameHeight, 'ascii', g).setOrigin(0, 0).setTint(c)
+            }
+          }
+
+          screenX++
+        }
+      }
+
+      screenX = 0
+      screenY++
+    }
+  }
+
+  processTurn() {
     const rogue = Game.entityManager.getRogue()
     let currentPosition = rogue.query('position')
     let moved = false
@@ -93,58 +149,6 @@ class MainScene extends Phaser.Scene {
       Game.entityManager
           .getEntitiesAtPosition(collisionX, collisionY)
           .forEach(e => e.receive('collidedWith', { collider: rogue }))
-    }
-  }
-
-  render() {
-    const rogue = Game.entityManager.getRogue()
-    const position = rogue.query('position')
-    const minX = position.x - ((this.cameraWidth - 1) / 2)
-    const maxX = position.x + ((this.cameraWidth - 1) / 2)
-    const minY = position.y - ((this.cameraHeight - 1) / 2)
-    const maxY = position.y + ((this.cameraHeight - 1) / 2)
-    
-    let screenX = 0
-    let screenY = 0
-
-    for (let y = minY; y <= maxY; y++) {
-      if (y >= 0 && y < Game.map.terrain.length) {
-        for (let x = minX; x <= maxX; x++) {
-          if (x >= 0 && x < Game.map.terrain[0].length) {
-            let tint
-    
-            switch (Game.map.terrain[y][x]) {
-              case 250:
-                tint = 0x3a3a3a
-                break
-              case 43:
-                tint = 0xa66b3a
-                break
-              default:
-                tint = 0x666666
-                break
-            }
-            
-            if (x !== position.x || y !== position.y) {
-              this.add.image(screenX * this.frameWidth, screenY * this.frameHeight, 'ascii', Game.map.terrain[y][x]).setOrigin(0, 0).setTint(tint)
-            }
-
-            const entityToRender = Game.entityManager.getRenderableEntityAtPosition(x, y)
-
-            if (entityToRender) {
-              let g = entityToRender.query('glyph')
-              let c = entityToRender.query('color')
-      
-              this.add.image(screenX * this.frameWidth, screenY * this.frameHeight, 'ascii', g).setOrigin(0, 0).setTint(c)
-            }
-          }
-
-          screenX++
-        }
-      }
-
-      screenX = 0
-      screenY++
     }
   }
 
