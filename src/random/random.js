92a09e64b1df983ddbcc35ef4e62d760edaa6100
Victory Softworks <sersavictory@gmail.com>
2021-11-28 00:28:33 -0500

    Added Random.choose static method

--- src/random/random.js
+++ src/random/random.js
@@ -30,4 +30,18 @@ class Random {
     return array[Math.floor(Math.random() * array.length)]
   }
 
+  /****************************************************************************
+   * Gets N random elements from the given array.
+   * 
+   * @param {Array} array Array to get random elements from
+   * @param {number} n Number of elements to get
+   * @returns {Array} Random elements
+   ***************************************************************************/
+
+  static choose(array, n) {
+    const shuffled = array.sort(() => 0.5 - Math.random())
+
+    return shuffled.slice(0, n)
+  }
+
 }
