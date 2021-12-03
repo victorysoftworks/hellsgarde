340752f3c51d15357a7c1541137346d39c75acb0
Victory Softworks <sersavictory@gmail.com>
2021-12-03 17:29:52 -0500

    Added squaresBetween method using Bresenham's line algorithm

--- src/geometry/geometry.js
+++ src/geometry/geometry.js
@@ -7,4 +7,24 @@ class Geometry {
     return Math.floor(Math.hypot(xDelta, yDelta))
   }
 
+  // Bresenham's line algorithm
+
+  static squaresBetween(x0, y0, x1, y1) {
+    var squares = []
+    var dx = Math.abs(x1 - x0), sx = x0 < x1 ? 1 : -1;
+    var dy = Math.abs(y1 - y0), sy = y0 < y1 ? 1 : -1; 
+    var err = (dx>dy ? dx : -dy)/2;
+  
+    while (true) {
+      squares.push([x0, y0])
+      
+      if (x0 === x1 && y0 === y1) break;
+      var e2 = err;
+      if (e2 > -dx) { err -= dy; x0 += sx; }
+      if (e2 < dy) { err += dx; y0 += sy; }
+    }
+
+    return squares
+  }
+
 }
